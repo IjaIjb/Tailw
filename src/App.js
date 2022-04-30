a085b21dc4532e13cc839eb0d825186ec692b5b6
IjaIj <ijabolu@gmail.com>
2022-04-30 18:11:10 +0100

    React tailwind project

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import Navbar from "./component/Navbar";
+import Hero from "./component/Hero";
+import About from "./component/About";
+import Support from "./component/Support";
+import AllInOne from "./component/AllIOne";
+import Pricing from "./component/Pricing";
+import Footer from './component/Footer';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+      <Support />
+      <AllInOne />
+      <Pricing />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
